2065cb8fb9141b9a424af38abce0d0aeae199067
Roberto Barahona <rabarahona1@espe.edu.ec>
2020-09-08 19:01:27 -0500

    Agregué el atributo invitations a la tabla Users para las notificaciones

--- PROJECT/BEU/Users.cs
+++ PROJECT/BEU/Users.cs
@@ -25,6 +25,7 @@ namespace BEU
         public string last_name { get; set; }
         public string email { get; set; }
         public string pass { get; set; }
+        public string invitations { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Board> Board { get; set; }
